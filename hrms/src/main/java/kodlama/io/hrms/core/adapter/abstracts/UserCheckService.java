9838d212dfaf2330371be514f753b4a9d3aff35a
nurseda diker <54947744+nurseda-diker@users.noreply.github.com>
2021-06-07 11:59:59 +0300

    Delete hrms directory

--- hrms/src/main/java/kodlama/io/hrms/core/adapter/abstracts/UserCheckService.java
+++
@@ -1,8 +0,0 @@
-package kodlama.io.hrms.core.adapter.abstracts;
-
-import kodlama.io.hrms.entities.concretes.JobSeekers;
-
-public interface UserCheckService {
-	
-	boolean checkIfRealPerson(JobSeekers jobSeeker);
-}
